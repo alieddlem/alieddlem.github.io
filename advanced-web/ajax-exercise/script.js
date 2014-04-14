9f16dfdec198fe01543c1986bfe6020e5dac2926
Ali Eddlem <aeddlem@yahoo.com>
2014-04-14 14:09:57 -0400

    got detail view to work yay

--- advanced-web/ajax-exercise/script.js
+++ advanced-web/ajax-exercise/script.js
@@ -1,15 +1,6 @@
 // Backup data: $.getJSON("http://web2spring2014.herokuapp.com/assets/dribbble-cc215791e1db5565d5f5ffa3cc4a3cc7.json",function(data) {
     $(function() {
-    	        function createShot(shots) {
-      var title = $("<div>").text(shots.title);
-      
-      $("#shots").append(title);
 
-      title.on("click",function() {
-        var img = $("<img>").attr("src",shots.image_url);
-        $("#detail").empty().append(img);
-      });
-    }
 
     $.getJSON("http://api.dribbble.com/shots/popular?callback=?",function(data) {
       console.log(data);
@@ -29,18 +20,29 @@ post.append(image);
 post.append(title);
 post.append(author);
 post.append(likes);
+ 
+ $("#shots").append(post);       
 
+createShot(data.shots[i]);        
+      
 }
 
-for(var i=0;i<data.shots.length;i++) {
+function createShot(shots) {
+
+      post.on("click",function() {
+
+        var img = $("<img>").attr("src",shots.image_url);
+        $("#detail").empty().append(img);
+      });
+    }
+
+
         // We need to save the current shot for later 
-        createShot(data.shots[i]);      	
-      }
 
 
 
 
-$("#shots").append(post);
+
 
 
 });
