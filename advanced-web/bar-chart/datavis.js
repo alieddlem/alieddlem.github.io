3be727b4b4b6950514a92e60ef942dbc28f0ee54
Ali Eddlem <aeddlem@yahoo.com>
2014-04-21 16:11:45 -0400

    bar chart stuff I never synced

---
+++ advanced-web/bar-chart/datavis.js
@@ -0,0 +1,27 @@
+$(function(){
+
+
+// Write our base functions
+console.log(data);
+
+//the loop      
+for(var i=0;i<data.length;i++){
+	var graph= data[i];
+	var person= $("<div class='person'>");
+
+
+var fullName = data[i][8];
+var salary = data[i][18] /200;
+
+
+var fullNameParagraph = $("<p>").text(fullName);
+var salaryParagraph = $("<p>").text(salary);
+var div= $("<div class='bar'></div>").css("width", salary);
+
+person.append(fullNameParagraph);
+person.append(div);
+
+$("#graph").append(person);
+}
+
+});
