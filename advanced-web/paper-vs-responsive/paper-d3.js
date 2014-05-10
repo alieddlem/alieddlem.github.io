69bb5960ddf49ae4fb89c226e28b013fb3b0d076
Ali Eddlem <aeddlem@yahoo.com>
2014-05-10 19:04:18 -0400

    created type 2 files

---
+++ advanced-web/paper-vs-responsive/paper-d3.js
@@ -0,0 +1,224 @@
+var paper = [
+
+{
+month: "2010-01-01",
+total: 15,
+adjusted: 225
+},
+{
+month: "2010-03-01",
+total: 28,
+adjusted: 255
+},
+{
+month: "2010-04-01",
+total: 31,
+adjusted: 234
+},
+{
+month: "2010-05-01",
+total: 26,
+adjusted: 285
+},
+{
+month: "2010-06-01",
+total: 70,
+adjusted: 352
+},
+{
+month: "2010-07-01",
+total: 63,
+adjusted: 326
+},
+{
+month: "2010-08-01",
+total: 66,
+adjusted: 256
+},
+{
+month: "2010-09-01",
+total: 51,
+adjusted: 225
+},
+{
+month: "2010-10-01",
+total: 67,
+adjusted: 213
+},
+{
+month: "2010-11-01",
+total: 65,
+adjusted: 220
+},
+{
+month: "2010-12-01",
+total: 59,
+adjusted: 235
+},
+{
+month: "2011-01-01",
+total: 89,
+adjusted: 241
+},
+{
+month: "2011-02-01",
+total: 82,
+adjusted: 235
+},
+{
+month: "2011-03-01",
+total: 142,
+adjusted: 258
+},
+{
+month: "2011-04-01",
+total: 128,
+adjusted: 261
+},
+{
+month: "2011-05-01",
+total: 178,
+adjusted: 272
+},
+{
+month: "2011-06-01",
+total: 126,
+adjusted: 225
+},
+{
+month: "2011-07-01",
+total: 137,
+adjusted: 205
+},
+{
+month: "2011-08-01",
+total: 191,
+adjusted: 244
+},
+{
+month: "2011-09-01",
+total: 176,
+adjusted: 220
+},
+{
+month: "2011-10-01",
+total: 152,
+adjusted: 193
+},
+{
+month: "2011-11-01",
+total: 183,
+adjusted: 199
+},
+{
+month: "2011-12-01",
+total: 231,
+adjusted: 243
+},
+{
+month: "2012-01-01",
+total: 312,
+adjusted: 255
+},
+{
+month: "2012-02-01",
+total: 291,
+adjusted: 204
+},
+{
+month: "2012-03-01",
+total: 293,
+adjusted: 218
+},
+{
+month: "2012-04-01",
+total: 264,
+adjusted: 190
+},
+{
+month: "2012-05-01",
+total: 289,
+adjusted: 190
+},
+{
+month: "2012-06-01",
+total: 236,
+adjusted: 172
+},
+{
+month: "2012-07-01",
+total: 237,
+adjusted: 159
+},
+{
+month: "2012-08-01",
+total: 252,
+adjusted: 174
+},
+{
+month: "2012-09-01",
+total: 209,
+adjusted: 152
+},
+{
+month: "2012-10-01",
+total: 251,
+adjusted: 152
+},
+{
+month: "2012-11-01",
+total: 246,
+adjusted: 160
+},
+{
+month: "2012-12-01",
+total: 202,
+adjusted: 146
+},
+{
+month: "2013-01-01",
+total: 271,
+adjusted: 148
+},
+{
+month: "2013-02-01",
+total: 248,
+adjusted: 136
+},
+{
+month: "2013-03-01",
+total: 213,
+adjusted: 110
+},
+{
+month: "2013-04-01",
+total: 276,
+adjusted: 141
+},
+{
+month: "2013-05-01",
+total: 269,
+adjusted: 133
+},
+{
+month: "2013-06-01",
+total: 211,
+adjusted: 112
+},
+{
+month: "2013-07-01",
+total: 189,
+adjusted: 107
+},
+{
+month: "2013-08-01",
+total: 165,
+adjusted: 101
+},
+{
+month: "2013-09-01",
+total: 31,
+adjusted: 105
+}
+
+];
---
+++ advanced-web/paper-vs-responsive/reponsive-d3.js
@@ -0,0 +1,149 @@
+var responsive = renderGraph(
+[
+{
+month: "2011-05-01",
+total: 17,
+adjusted: 26
+},
+{
+month: "2011-06-01",
+total: 20,
+adjusted: 36
+},
+{
+month: "2011-07-01",
+total: 21,
+adjusted: 31
+},
+{
+month: "2011-08-01",
+total: 43,
+adjusted: 55
+},
+{
+month: "2011-09-01",
+total: 41,
+adjusted: 51
+},
+{
+month: "2011-10-01",
+total: 50,
+adjusted: 64
+},
+{
+month: "2011-11-01",
+total: 43,
+adjusted: 47
+},
+{
+month: "2011-12-01",
+total: 53,
+adjusted: 56
+},
+{
+month: "2012-01-01",
+total: 113,
+adjusted: 92
+},
+{
+month: "2012-02-01",
+total: 124,
+adjusted: 87
+},
+{
+month: "2012-03-01",
+total: 141,
+adjusted: 105
+},
+{
+month: "2012-04-01",
+total: 142,
+adjusted: 102
+},
+{
+month: "2012-05-01",
+total: 174,
+adjusted: 114
+},
+{
+month: "2012-06-01",
+total: 154,
+adjusted: 112
+},
+{
+month: "2012-07-01",
+total: 182,
+adjusted: 122
+},
+{
+month: "2012-08-01",
+total: 231,
+adjusted: 160
+},
+{
+month: "2012-09-01",
+total: 192,
+adjusted: 140
+},
+{
+month: "2012-10-01",
+total: 253,
+adjusted: 154
+},
+{
+month: "2012-11-01",
+total: 264,
+adjusted: 172
+},
+{
+month: "2012-12-01",
+total: 229,
+adjusted: 166
+},
+{
+month: "2013-01-01",
+total: 417,
+adjusted: 228
+},
+{
+month: "2013-02-01",
+total: 449,
+adjusted: 246
+},
+{
+month: "2013-03-01",
+total: 425,
+adjusted: 220
+},
+{
+month: "2013-04-01",
+total: 443,
+adjusted: 227
+},
+{
+month: "2013-05-01",
+total: 460,
+adjusted: 227
+},
+{
+month: "2013-06-01",
+total: 474,
+adjusted: 251
+},
+{
+month: "2013-07-01",
+total: 442,
+adjusted: 251
+},
+{
+month: "2013-08-01",
+total: 434,
+adjusted: 266
+},
+{
+month: "2013-09-01",
+total: 65,
+adjusted: 220
+}
+]
+)
