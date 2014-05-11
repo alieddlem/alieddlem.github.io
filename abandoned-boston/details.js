4a1b70ee8ec2e3e95e3da664197e6c24d2e2ff8c
Ali Eddlem <aeddlem@yahoo.com>
2014-05-10 20:52:14 -0400

    finished splash page sorta

---
+++ abandoned-boston/details.js
@@ -0,0 +1,50 @@
+var Buildings = {
+
+  "building-1": {
+    title: "Highland Springs Brewery",
+    image: "",
+    description:  "Sitting at 53 New Heath Street is the abandoned Highland Spring Brewery. This Georgian Revival building was built in 1892. Highland Spring Brewery was founded by Henry A. Reuter and John R Alley, two brewers from Germany and Dublin, in 1876. Its success led to the construction of the late Victorian building at 165 Terrace Street, and later, the Georgian Revival addition on New Heath Street. This brewery is noted for being one of the first breweries to add refrigeration mach-inery to its plant. Prohibition led to their closing, and in 1925, Oliver Ditson Co., Boston’s largest sheet music publisher, purchased the Georgian Revival addition for printing and storage. These buildings have not been used since the 1980’s. The New England Baptist Hospital uses the adjacent parking lot to transport patients to the hospital, but claim to know nothing about the forgotten brewery, and deny owning any keys.",
+  },
+  "building-2": {
+    title: "Eblana Brewery",
+    image: "",
+    description:  "Eblana Brewery can be found at 123–127 Heath street. This brewery was 
+founded by John R. Alley once he split with Reuter in 1885. One year later, 
+he built the Heath Street property. This brewery, along with many other Boston breweries, closed with prohibition and became a wool factory through the 1920’s. During the 1950’s, it was used as a soda bottling factory, and finally an automobile factory in 1960. If the gaping windows surrounded by crumbling brick don’t signify the deterioration of this once industrial legend, the condemning “X” on the front certainly does. It seems strange that this building has been abandoned for merely three years. The premises is surrounded by a tall barbed wire fence, and shows no signs of entry from the street. Inside, there are two large freight elevators, rusted with time and age, and four floors of large industrial-sized rooms. At the top of the staircases, here are ladders that lead up to the slanted roof."
+},
+  "building-3": {
+    title: "Christian Bibleway Center",
+    image: "",
+    description:  "This structure was built in 1910, and served as an industrial building for years before it was converted into a church. Bibleway Christian Center Inc bought this property in the early 2000’s, and began work on the interior without residential approval. This led to an outcry in the community and several hearings. Bibleway Christian Center Inc moved to another location in Dorchester, and now this building stand abandoned once more. The neighbors will have their silence."
+},
+  "building-4": {
+    title: "St. Alphonsus Hall",
+    image: "",
+    description:  "St. Alphonsus Hall was built in 1898. It was designed as “a clubhouse for the young men of the parish,” and included a library, recreation room, theater, 
+and bowling alleys. The building has been unoccupied for twenty to thirty years. Repairs began in the past few years on this building, and builders have taken great care in leaving the architecture similar to the original design. The plaster has been stripped from the walls and columns. The seats have been removed, and the building itself has been generally cleared of debris. A few planks hang from the exposed gorgeously curved ceiling, and some fallen bricks litter the floors. The floor in the lobbies is marble tiled, and the main staircases in the front of the theater are made as marble as well. The entrance to the property is incredibly regal, with marble walls and steps, and the remains of stained glass windows above the double doors. There lies high ceilinged basement below the theater, now full of puddles and dirt. There are several oddly-shaped rooms in the back of the basement, one of which holds eight stone shower stalls."
+},
+  "building-5": {
+    title: "The Convent of Our Lady of Perpetual Help",
+    image: "",
+    description:  "The Convent of Our Lady of Perpetual Help was built in 1888 to serve the Sisters of Notre Dame and other educators in the area associated with the church. A third floor and roof terrace addition was built in 1901. Aside from a single room where the children from Mission Grammar school sometimes wait, this building has gone unused for 20 to 30 years. There are rumors of a tunnel that runs under this building, connecting it to the Mission School and St. Alphonsus."
+},
+  "building-6": {
+    title: "6–12 Pontiac Street",
+    image: "",
+    description:  "Nestled in the suburbs of Mission Hill, this building stands one story tall. It has been abandoned for fifteen years, and is covered with graffiti and odd markings. John Darling, the property’s owner, owns several other properties in Roxbury and Jamaica plain that are visibly distressed. Darling has made 
+no comments on these properties, but has been taken to court several times over found squatters, and ordered to secure these properties better. The Pontiac street properties are secured by an aluminum fence and a stone wall. The small commercial building visible from the street is impenetrable due to new security, but the residential building behind it has several broken windows, and a root growing through the boarded-up garage that leaves a small opening. My team and I were able to get pictures through these holes in the building. The photos show rooms that have surely been accessed recently. Graffiti covers the inner walls, and old dusty chairs are placed in each room, along with various frames and trash bins. A large oil tank, also covered in graffiti, is tipped over in one incredibly distressed room.  The grass has been recently cut and leaves and twigs have been gathered into trash bags. I can only assume Darling has come around enough to complete the minimum amount of work required in order to avoid another court case."
+},
+  "building-7": {
+    title: "Paul's New & Used Furniture",
+    image: "",
+    description:  "434 Blue Hill Ave was opened in 1990 as a new and used furniture store. Paul’s New&Used Furniture was owned and operated by Paul Williams until he had to close two years ago. According to online records, he was the sole employee. When we arrived on the property, it was secured by an 8 foot tall barbed wire fence. The plaster on the interior is almost entirely scraped away, showing exposed brick and piping. The few relics left inside were random at best. There was a box of VHS pornography found next to old armchair. Also found inside were an oven, a white wooden closet filled with trash, and a cardboard box filled with roughly 100 copies of Shakespeare’s “Measure For Measure.” On top of an unplugged mini fridge (which was empty inside) there was a journal with architectural drawings inside. These drawings seemed to match the drawings of the building itself. Perhaps they were renovation drawings. Aside from the few pieces of furniture left, which may or may not have been merchandise of the store, the sole item left from the retail company was the open/closed sign still hanging on the door. The layer of dust covering all of the articles in the single-room building suggests that there has not been construction in this building
+ for quite some time."
+},
+  "building-8": {
+    title: "The Broadway Theater",
+    image: "",
+    description:  "The Broadway theater stands at 420 Broadway street in South Boston. 
+The theater was built in 1920 by architects Clarence H Blackall, Clapp and Whittemore. After a fire, it was rebuilt in 1938. In the fifties, the two sets of large double doors were set back roughly twenty feet from the street. There were cases on each side of the doors for movie posters. On Saturday afternoons, it was twenty-five cents to see five cartoons, two or three Stooges shorts, and a feature length film. Every other day, it showed reruns of regular Hollywood films.In the early seventies, the main theater was split to make two screens. The Broadway Theater closed in 1982, and since then has slowly begun to deteriorate. The ceilings have begun to fall in, and several windows in the back are gaping from fallen wooden panels and crumbling brick. Although these buildings have been 
+left to rot, there may still be hope for many of them. In recent years, the City of Boston has been much more strict when dealing with distressed buildings. Perhaps these owners will finally tear down their condemned properties, or perhaps they will finally restore them. One can only hope that these properties will maintain some pieces of their past as they are restored for the future. For the residents of the city, the history behind these structures is crucial to the integrity of their neighborhood."
+},
+}
