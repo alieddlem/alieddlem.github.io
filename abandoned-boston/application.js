ddcea8b5c49f0929c3a5238070d49cb7937dcfbc
Ali Eddlem <aeddlem@yahoo.com>
2014-05-10 23:12:53 -0400

    fixed a lotta sturf

--- abandoned-boston/application.js
+++ abandoned-boston/application.js
@@ -1,4 +1,33 @@
-function showBuilding(name){
+function hideSplash(name){
+	$("#explore-button").fadeOut("slow");
+	$(".word").fadeOut("slow");
+	$(".overlay").fadeOut("slow");
+	
+}
+
+function showMainPageItems(name){
+	$("#windows").fadeIn("slow");
+	$("#half-title").fadeIn("slow");
 
 	
 }
+
+
+
+
+// When the page loads, add in our event handlers
+$(function() {
+$("#explore-button").click(function() {
+	hideSplash();
+	showMainPageItems();
+	});
+
+
+
+
+
+
+
+
+
+});
